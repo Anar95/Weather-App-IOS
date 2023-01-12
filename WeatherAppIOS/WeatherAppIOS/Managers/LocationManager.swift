20e54cdd5cbd0fbad2534bea8bb7d8b467e4bb31
Anar95 <anarabbas458@gmail.com>
2023-01-12 23:53:05 +0300

    First Update

---
+++ WeatherAppIOS/WeatherAppIOS/Managers/LocationManager.swift
@@ -0,0 +1,38 @@
+//
+//  LocationManager.swift
+//  WeatherAppIOS
+//
+//  Created by Anar Abbas on 12.01.2023.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager:NSObject, ObservableObject, CLLocationManagerDelegate{
+    let manager =CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isloading = false
+    
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    func requestLocation(){
+        isloading = true
+        manager.requestLocation()
+        
+    }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isloading = false
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location", error)
+        isloading = false
+    }
+    
+}
---
+++ WeatherAppIOS/WeatherAppIOS/WeatherAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppIOSApp.swift
+//  WeatherAppIOS
+//
+//  Created by Anar Abbas on 12.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
